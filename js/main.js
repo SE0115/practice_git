4b4d24f4c55b40e6ff4a6466c4a39b09b36b1147
sieun <sieun0115@ajou.ac.kr>
2021-07-30 14:36:34 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,70 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function() {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500){
+        // 배지 숨기기
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+        
+    } else{
+        // 배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300)); //0.3초 단위로 부하를 주어 함수가 우르르 실행되는 것을 방지. throttle : 일정시간에 한번씩만 실행되도록 제한. 
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index+1) * .7,
+        opacity: 1
+    });
+});
+
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+    /* direction: 'horizontal' => 기본값으로 지정되어 있어 따로 명시할 필요 x */
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000 // ms 단위
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true // 사용자의 페이지 번호 요소 제어
+    },   
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
